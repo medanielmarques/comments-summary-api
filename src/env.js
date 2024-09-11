b7d90c2bed9483f60fe3b600bcbf78bdb110cb8f
Daniel Marques <daniel.brz2009@gmail.com>
2024-09-11 15:36:37 -0300

    yeah

--- src/env.js
+++ src/env.js
@@ -6,27 +6,26 @@ export const env = createEnv({
     NODE_ENV: z
       .enum(["development", "test", "production"])
       .default("development"),
+
     DATABASE_URL: z.string(),
     DATABASE_TOKEN: z.string().optional(),
-  },
 
-  client: {
-    NEXT_PUBLIC_SUPABASE_URL: z.string(),
-    NEXT_PUBLIC_SUPABASE_ANON_KEY: z.string(),
-
-    // NEXT_PUBLIC_HIGHLIGHT_PROJECT_ID: z.string(),
+    SUPABASE_URL: z.string(),
+    SUPABASE_ANON_KEY: z.string(),
+    // HIGHLIGHT_PROJECT_ID: z.string(),
   },
 
   runtimeEnv: {
     NODE_ENV: process.env.NODE_ENV,
+
     DATABASE_URL: process.env.DATABASE_URL,
     DATABASE_TOKEN: process.env.DATABASE_TOKEN,
 
-    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
-    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+    SUPABASE_URL: process.env.SUPABASE_URL,
+    SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY,
 
-    // NEXT_PUBLIC_HIGHLIGHT_PROJECT_ID:
-    //   process.env.NEXT_PUBLIC_HIGHLIGHT_PROJECT_ID,
+    // HIGHLIGHT_PROJECT_ID:
+    //   process.env.HIGHLIGHT_PROJECT_ID,
   },
 
   skipValidation: !!process.env.SKIP_ENV_VALIDATION,
