ff8166a2a83481aadb8cf1a8528565f71fa0bb22
Daniel Marques <daniel.brz2009@gmail.com>
2024-09-12 21:43:36 -0300

    yeah

--- api/create-summary.ts
+++ api/create-summary.ts
@@ -1,52 +1,26 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
-import { google } from "googleapis";
 import OpenAI from "openai";
-import { db } from "../src/db/index.js";
-import { commentsSummary, summaryCommentIds } from "../src/db/schema.js";
 import { env } from "../src/env.js";
+import { Redis } from "@upstash/redis";
 
-// Define types for comments
 interface Comment {
   commentId: string;
   comment: string;
 }
 
-// Enumerate comments function with types
 function enumerateComments(comments: string[]): string {
   return comments.map((comment, index) => `${index + 1}. ${comment}`).join(" ");
-}
-
-// Fetch comments from YouTube API
-async function fetchComments(videoId: string): Promise<Comment[]> {
-  const youtube = google.youtube({
-    version: "v3",
-    auth: env.YOUTUBE_DATA_API_KEY,
-  });
-
-  try {
-    const commentsResponse = await youtube.commentThreads.list({
-      part: ["snippet"],
-      videoId,
-      maxResults: 50,
-      order: "relevance",
-    });
-
-    return (
-      commentsResponse.data.items?.map((item) => ({
-        commentId: item.snippet?.topLevelComment?.id ?? "",
-        comment: item.snippet?.topLevelComment?.snippet?.textOriginal ?? "",
-      })) ?? []
-    );
-  } catch (error) {
-    console.error("Error fetching comments:", error);
-    return [];
-  }
 }
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   const videoId = "LSo7xH7YgaY";
 
-  const comments = await fetchComments(videoId);
+  const redis = new Redis({
+    url: env.UPSTASH_REDIS_REST_URL,
+    token: env.UPSTASH_REDIS_REST_TOKEN,
+  });
+
+  const comments = await redis.smembers(`video:${videoId}:comments-content`);
   const openai = new OpenAI({ apiKey: env.OPENAI_API_KEY });
 
   try {
@@ -56,43 +30,25 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
       messages: [
         {
           role: "user",
-          content: `
-          Summarize the 4 main topics on this comments, keep in mind that content creators will be the ones reading this, so adapt the text accordingly and make it as useful as possible for them. ${enumerateComments(
-            comments.map((c) => c.comment)
+          content: ` Summarize the 4 main topics on this comments, keep in mind that content creators will be the ones reading this, so adapt the text accordingly and make it as useful as possible for them. ${enumerateComments(
+            comments
           )}`,
         },
       ],
     });
 
-    // Insert the summary into the database
-    const [commentSummary] = await db
-      .insert(commentsSummary)
-      .values({
-        summary: completion.choices[0]?.message?.content || "",
-        videoId,
-        userId: "ctx.user!.id",
-        // userId: ctx.user!.id,
-      })
-      .returning({ id: commentsSummary.id });
+    const summary = completion.choices[0]?.message?.content || "";
 
-    // Insert comment IDs into the summaryCommentIds table
-    await Promise.all(
-      comments.map(async (comment) => {
-        await db.insert(summaryCommentIds).values({
-          comment_id: comment.commentId,
-          comment_summary_id: commentSummary?.id,
-        });
-      })
-    );
+    await redis.hset(`video:${videoId}:summary`, {
+      videoId,
+      summary,
+    });
 
     res.status(200).json({
-      message: "Hello world!",
-      cookies: req.cookies,
+      message: "Summary created successfully.",
     });
   } catch (error) {
     console.error("Error with OpenAI API:", error);
-    res.status(400).json({
-      message: "Failed to create summary",
-    });
+    res.status(400).json({ message: "Failed to create summary" });
   }
 }
